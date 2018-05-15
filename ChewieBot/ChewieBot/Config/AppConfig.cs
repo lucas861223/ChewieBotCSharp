9dd884eaaf7ffe75aefb7381cb4253c3d4fa4cb1
Magenta <i.snowblind7@gmail.com>
2018-05-15 06:48:37 +0100

    Initial setup. Connect to a twitch channel.

---
+++ ChewieBot/ChewieBot/Config/AppConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChewieBot.Config
+{
+    public static class AppConfig
+    {
+        public static string TwitchOAuth = ConfigurationManager.AppSettings["Twitch-OAuth"];
+        public static string TwitchUsername = ConfigurationManager.AppSettings["Twitch-Username"];
+        public static string TwitchChannel = ConfigurationManager.AppSettings["Twitch-Channel"];
+    }
+}
---
+++ ChewieBot/ChewieBot/Twitch/TwitchClient.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client.Models;
+using ChewieBot.Config;
+using TwitchLib.Client.Events;
+
+namespace ChewieBot.Twitch
+{
+    public class TwitchClient
+    {
+        private TwitchLib.Client.TwitchClient client;
+
+        public TwitchClient()
+        {
+
+        }
+
+        public void Start()
+        {
+            var credentials = new ConnectionCredentials(AppConfig.TwitchUsername, AppConfig.TwitchOAuth);
+
+            client = new TwitchLib.Client.TwitchClient();
+            client.Initialize(credentials, AppConfig.TwitchChannel);
+
+            client.OnMessageReceived += OnMessageReceived;
+            client.OnJoinedChannel += OnJoinedChannel;
+
+            client.Connect();
+        }
+
+        private void OnMessageReceived(object sender, OnMessageReceivedArgs e)
+        {
+            client.SendMessage(e.ChatMessage.Channel, $"Message!! -- {e.ChatMessage.Message}");
+        }
+
+        private void OnJoinedChannel(object sender, OnJoinedChannelArgs e)
+        {
+            client.SendMessage(e.Channel, $"Joined channel!!");
+        }
+    }
+}
